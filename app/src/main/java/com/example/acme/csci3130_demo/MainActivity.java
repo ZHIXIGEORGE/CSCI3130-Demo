9cb2b839fe82672b2ed494e783f08ed51d7d3a1c
ZHIXIGEORGE <35536765+ZHIXIGEORGE@users.noreply.github.com>
2018-01-22 15:56:09 -0500

    Add a button, textEdit and  modify layout
    followed by requirements on the assignment1 part3

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText inputTfield;
+    private TextView text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        inputTfield = (EditText) findViewById(R.id.inputTfield);
+        text1 = (TextView) findViewById(R.id.text1);
+
+        button.setOnClickListener(new View.OnClickListener() {//this method will set textview value to edittext
+            @Override
+            public void onClick(View view) {
+                text1.setText(inputTfield.getText());
+            }
+        });
     }
 }
